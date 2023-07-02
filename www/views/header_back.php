bb393110a84f1f2220af608ee3c50bf7d067be25
johnnyhelloworld <johnny58723826@hotmail.fr>
2023-07-02 16:56:15 +0200

    CRUD for Categorie

--- www/views/header_back.php
+++ www/views/header_back.php
@@ -6,6 +6,7 @@
             <li><a href="/admin/users">Manage Users</a></li>
             <li><a href="/admin/pages">Manage Pages</a></li>
             <li><a href="/admin/recipes">Manage Recipes</a></li>
+            <li><a href="/admin/categories">Manage Categories</a></li>
             <li><a href="/admin/settings">Settings</a></li>
         </ul>
     </nav>
