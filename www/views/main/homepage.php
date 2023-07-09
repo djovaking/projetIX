d273c805a80f839d333d4a10746bfdd2c227f798
Yolo <djovaking@gmail.com>
2023-07-09 13:47:11 +0200

    style de la homepage mis a jour avec quelques sections

--- www/views/main/homepage.php
+++ www/views/main/homepage.php
@@ -1,2 +1,17 @@
-<h2>Page d'accueil</h2>
-<h3><?= $message ?></h3>
+<section class="hero-banner">
+    <div class="hero-content">
+        <h1 class="hero-title">Welcome to Our Food Website</h1>
+        <p class="hero-description">Explore the world of delicious food and recipes</p>
+        <a href="#" class="hero-button">Discover More</a>
+    </div>
+</section>
+<section class="about-us">
+    <div class="about-us-content">
+        <h2>About Us</h2>
+        <p>Welcome to FoodPress, a platform dedicated to celebrating the love for food and culinary exploration.</p>
+        <p>At Foodpress, we believe that food is more than just sustenance; it's a way of life. We are passionate about creating and sharing delectable recipes, culinary tips, and the joy of cooking.</p>
+        <p>Our team of experienced chefs, food enthusiasts, and writers work diligently to curate a collection of mouth-watering recipes, informative articles, and practical cooking advice. We aim to inspire home cooks, food lovers, and aspiring chefs to embark on their culinary journey.</p>
+        <p>Whether you're a novice in the kitchen or a seasoned chef, our goal is to provide you with a trusted resource to explore, experiment, and elevate your culinary skills.</p>
+        <p>Join us on this gastronomic adventure and let's celebrate the art of cooking together!</p>
+    </div>
+</section>
