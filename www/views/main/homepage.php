95d5acc09566c71c107cc70033e5b0672cdfd37d
thomasR92 <thomas.riehl92@gmail.com>
2023-07-10 01:29:38 +0200

    homepage

--- www/views/main/homepage.php
+++ www/views/main/homepage.php
@@ -1,2 +1,19 @@
-<h2>Page d'accueil</h2>
-<h3><?= $message ?></h3>
+<div class="container-banner">
+    <img class="background-image" src="../assets/images/banner.jpg" alt="Background Image">
+    <div class="text-overlay">
+        Bienvenue sur FoodPress
+    </div>
+</div>
+
+<div class="section-banner">
+    <div class="section-banner-text">
+        <p>
+            Bienvenue sur FoodPress, la plateforme CMS pour restaurateurs ! <br>
+            Créez et gérez facilement votre site web professionnel pour votre restaurant. Personnalisez votre design, mettez en valeur vos plats et simplifiez la réservation en ligne. <br>
+            Interagissez avec vos clients, intégrez les réseaux sociaux et suivez les performances de votre site, le tout depuis une interface conviviale. <br>
+            FoodPress simplifie votre présence en ligne, stimule les réservations et vous aide à réussir dans le secteur de la restauration. <br>
+            Rejoignez FoodPress dès aujourd'hui et découvrez comment notre plateforme CMS peut booster votre restaurant !
+        </p>
+
+    </div>
+</div>
