a75ce7bee6162f0caadd1f9daf17bc37852fed5f
Rashmi Bhavsar <94361221+rash2615@users.noreply.github.com>
2023-07-09 14:56:23 +0200

    CRUD Reservation from feature-reservation branch

--- www/controllers/Front.php
+++ www/controllers/Front.php
@@ -30,4 +30,28 @@ final class Front
     {
         $view = new View("main/contact", "front");
     }
+
+    public function reservation()
+    {   
+        $form = new ReservationForm();
+        if($form->isSubmited() && $form->isValid()){
+            //Create a new Reservation object
+            $reservation = new Reservation();
+            //Set the reservation object's properties with form data
+            $reservation->setFirstName($_POST['firstname']);
+            $reservation->setLastName($_POST['lastname']);
+            $reservation->setPhone($_POST['phone']);
+            $reservation->setDate($_POST['date']);
+            $reservation->setTime($_POST['time']);
+            $reservation->setnb_person($_POST['nb_person']);
+            
+            // Call the save() method to save the reservation object into the database
+            $reservation->save();
+            echo "OK";
+        }
+       
+        $view = new View("main/reservation", "front");
+        $view->assign('form', $form->getConfig());
+        $view->assign('formErrors', $form->listOfErrors);
+    }
 }
