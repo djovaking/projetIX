efcf13044c6d5a9cd162d108269d0143d8289a75
Rashmi Bhavsar <94361221+rash2615@users.noreply.github.com>
2023-07-09 15:37:52 +0200

    Ajout formulaire de contact

--- www/views/main/contact.php
+++ www/views/main/contact.php
@@ -1,1 +1,14 @@
 <h2>page de contact</h2>
+
+<form method="post" action="">
+        <label for="nom">Nom:</label>
+        <input type="text" name="nom" required><br>
+
+        <label for="email">Email:</label>
+        <input type="email" name="email" required><br>
+
+        <label for="message">Message:</label><br>
+        <textarea name="message" rows="5" required></textarea><br>
+
+        <input type="submit" name="submit" value="Envoyer">
+</form>
